05f741973947e30db4243fc1bb050ce0df4184d2
NodEmKay <mkaydevops@gmail.com>
2026-01-30 20:11:30 +0000

    Configure Terraform backend and reduce instance count

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "NodeEmkay"
+
+    workspaces {
+      name = "Terraform"
+    }
+  }
+}
 
 resource "aws_instance" "example" {
-  count         = 4
+  count         = 2
   ami           = "ami-0532be01f26a3de55" # Amazon Linux 2 AMI (update based on region)
   instance_type = "t2.micro"
 
