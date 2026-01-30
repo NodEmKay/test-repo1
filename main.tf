cc235ff6e88711a1afaa48500ceba2e2e7b36083
NodEmKay <mkaydevops@gmail.com>
2026-01-30 19:37:42 +0000

    Increase AWS instance count to 4

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+  count         = 4
   ami           = "ami-0532be01f26a3de55" # Amazon Linux 2 AMI (update based on region)
   instance_type = "t2.micro"
 
